cb1055181da76fd3523da229bf872a86f13ba8b4
Cary <caryando@gmail.com>
2016-01-14 22:15:03 -0800

    Add a little boilerplate and ViewPager with dummy fragments

--- app/src/main/java/com/piedpiper1337/pickwhich/activities/HomeActivity.java
+++ app/src/main/java/com/piedpiper1337/pickwhich/activities/HomeActivity.java
@@ -5,22 +5,31 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 import com.piedpiper1337.pickwhich.R;
+import com.piedpiper1337.pickwhich.adapters.InboxFragmentPagerAdapter;
 import com.piedpiper1337.pickwhich.callbacks.RESTApiBroadcastReceiver;
 import com.piedpiper1337.pickwhich.callbacks.RESTApiProcessorCallback;
+import com.piedpiper1337.pickwhich.fragments.InboxFragment;
+import com.piedpiper1337.pickwhich.fragments.dummy.DummyContent;
 import com.piedpiper1337.pickwhich.utils.Constants;
 
-public class HomeActivity extends BaseActivity implements RESTApiProcessorCallback {
+public class HomeActivity extends BaseActivity implements RESTApiProcessorCallback, InboxFragment.OnListFragmentInteractionListener {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
 
     // To subscribe to broadcasts
     private RESTApiBroadcastReceiver mReceiver;
     private IntentFilter mFilter;
+
+    private ViewPager mInboxViewPager;
+    private InboxFragmentPagerAdapter mInboxFragmentPagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     public String getTag() {
@@ -50,6 +59,13 @@ public class HomeActivity extends BaseActivity implements RESTApiProcessorCallba
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
     public void onHttpResponseError(Intent intent) {
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
@@ -65,12 +81,6 @@ public class HomeActivity extends BaseActivity implements RESTApiProcessorCallba
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
         }
-
-        if (intent.getIntExtra(Constants.IntentExtras.REQUEST_ID, -1) == Constants.ApiRequestId.LOGIN) {
-            // Successfully logged in, continue to next screen
-            startActivity(new Intent(this, HomeActivity.class)); // Start the main app Activity
-            finish();
-        }
     }
 
     private void initUI() {
@@ -83,6 +93,23 @@ public class HomeActivity extends BaseActivity implements RESTApiProcessorCallba
                         .setAction("Action", null).show();
             }
         });
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        mInboxViewPager = (ViewPager) findViewById(R.id.inbox_viewpager);
+        mInboxFragmentPagerAdapter = new InboxFragmentPagerAdapter(getSupportFragmentManager(), this);
+        mInboxFragmentPagerAdapter.addFragment(InboxFragment.newInstance(1), "Inbox", R.drawable.ic_inbox_white_24dp);
+        mInboxFragmentPagerAdapter.addFragment(InboxFragment.newInstance(1), "Sent", R.drawable.ic_inbox_white_24dp);
+        mInboxViewPager.setAdapter(mInboxFragmentPagerAdapter);
+
+        mTabLayout.setupWithViewPager(mInboxViewPager);
+
+        mTabLayout.getTabAt(0).setIcon(R.drawable.ic_inbox_white_24dp);
+        mTabLayout.getTabAt(1).setIcon(R.drawable.ic_send_white_24dp);
     }
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 }
